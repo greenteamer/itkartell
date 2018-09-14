d146153ebc337b6628a20983871ff5406b01be32
Unknown <teamer777@gmail.com>
2018-09-14 04:00:08 +0300

    simple html & static with express

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use('/static', express.static('dist'));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
 app.listen(3000, () => console.log('App starts in port 3000'));
